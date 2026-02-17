f23c22a0cee84e7f031e54df132e44ef93aae8c5
StefanVNikolov <stef.nikolov@outlook.com>
2026-02-17 13:33:22 +0900

    fix: Correct PuzzleScreen import name in screens module

--- dco/ui/screens/__init__.py
+++ dco/ui/screens/__init__.py
@@ -3,7 +3,7 @@ from .home import HomeScreen
 from .library import LibraryScreen
 from .play import PlayScreen
 from .practice import PracticeScreen
-from .puzzles import PuzzlesScreen
+from .puzzles import PuzzleScreen
 from .statistics import StatisticsScreen
 
-__all__ = ["HomeScreen", "LibraryScreen", "PlayScreen", "PracticeScreen", "PuzzlesScreen", "StatisticsScreen"]
+__all__ = ["HomeScreen", "LibraryScreen", "PlayScreen", "PracticeScreen", "PuzzleScreen", "StatisticsScreen"]
